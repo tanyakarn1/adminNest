4b4c5bea2cc9102e8d2c0f4521af8f628649d3d2
akki01133 <ajeetkumar83589@gmail.com>
2024-04-03 15:22:14 +0530

    fixed user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 
 
 
 @Module({
-  imports: [],
+  imports: [
+    // initialize the mongoose module
+    MongooseModule.forRoot('mongodb://localhost:27017/adminnest'),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
   
